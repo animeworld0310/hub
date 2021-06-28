6b08c5930729b97fbed877259e7d8a62a820451f
animeworld0310 <73987648+animeworld0310@users.noreply.github.com>
2021-06-29 00:58:52 +0530

    Create WebDesign.js

---
+++ WebDesign.js
@@ -0,0 +1,25 @@
+let tabs = document.querySelector(".tabs");
+
+let tabHeader = tabs.querySelector(".tab-header");
+
+let tabHeaderElements = tabs.querySelectorAll(".tab-header > div");
+
+let tabBody = tabs.querySelector(".tab-body");
+
+let tabBodyElements = tabs.querySelectorAll(".tab-body > div");
+
+for(let i=0;i<tabHeaderElements.length;i++){
+
+  tabHeaderElements[i].addEventListener("click",function(){
+
+    tabHeader.querySelector(".active").classList.remove("active");
+
+    tabHeaderElements[i].classList.add("active");
+
+    tabBody.querySelector(".active").classList.remove("active");
+
+    tabBodyElements[i].classList.add("active");
+
+  });
+
+}
